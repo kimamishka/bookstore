d0c915f3facf68f418eba65610538b99d2777822
kimamishka <66490635+kimamishka@users.noreply.github.com>
2020-11-08 10:15:09 +0300

    добавила страницы в bestsellers

--- js/script.js
+++ js/script.js
@@ -26,29 +26,92 @@ $(".li__11").click(function (event) {
 
 //tabs index
 $(".one__na").click(function (event) {
-  $("#newarriv").css("display", "block");
-  $("#bestsel, #used, #special").css("display", "none");
+  $(".newarriv").css("display", "block");
+  $(".bestsel, .used, .special").css("display", "none");
   $(".two__bs, .two__na, .one__ub, .one__so").css("display", "inline-block");
   $(".one__bs, .one__na, .two__ub, .two__so").css("display", "none");
 });
 $(".one__ub").click(function (event) {
-  $("#used").css("display", "block");
-  $("#bestsel, #newarriv, #special").css("display", "none");
+  $(".used").css("display", "block");
+  $(".bestsel, .newarriv, .special").css("display", "none");
   $(".two__bs, .one__na, .two__ub, .one__so").css("display", "inline-block");
   $(".one__bs, .two__na, .one__ub, .two__so").css("display", "none");
 });
 $(".one__so").click(function (event) {
-  $("#special").css("display", "block");
-  $("#bestsel, #used, #newarriv").css("display", "none");
+  $(".special").css("display", "block");
+  $(".bestsel, .used, .newarriv").css("display", "none");
   $(".two__bs, .one__na, .one__ub, .two__so").css("display", "inline-block");
   $(".one__bs, .two__na, .two__ub, .one__so").css("display", "none");
 });
 $(".two__bs").click(function (event) {
-  $("#bestsel").css("display", "block");
-  $("#newarriv, #used, #special").css("display", "none");
+  $(".bestsel").css("display", "block");
+  $(".newarriv, .used, .special").css("display", "none");
   $(".one__bs, .one__na, .one__ub, .one__so").css("display", "inline-block");
   $(".two__bs, .two__na, .two__ub, .two__so").css("display", "none");
 });
+
+//namberpage
+$("#p1").click(function (event) {
+  $("#p1").css("border-right", "1px solid #d8dce0");
+  $("#p1").css("border-bottom", "4px solid #81bd66");
+  $("#p1").css("opacity", "0.8");
+  $("#p2,#p3,#p4,#p5,#p6").css("border", "none");
+  $("#p2,#p3,#p4,#p5,#p6").css("opacity", "1");
+
+  $("#bestsel_1").css("display", "block");
+  $("#bestsel_2, #bestsel_3, #bestsel_4, #bestsel_5, #bestsel_6").css("display", "none");
+});
+$("#p2").click(function (event) {
+  $("#p2").css("border-right", "1px solid #d8dce0");
+  $("#p2").css("border-bottom", "4px solid #81bd66");
+  $("#p2").css("opacity", "0.8");
+  $("#p1,#p3,#p4,#p5,#p6").css("border", "none");
+  $("#p1,#p3,#p4,#p5,#p6").css("opacity", "1");
+
+  $("#bestsel_2").css("display", "block");
+  $("#bestsel_1, #bestsel_3, #bestsel_4, #bestsel_5, #bestsel_6").css("display", "none");
+});
+$("#p3").click(function (event) {
+  $("#p3").css("border-right", "1px solid #d8dce0");
+  $("#p3").css("border-bottom", "4px solid #81bd66");
+  $("#p3").css("opacity", "0.8");
+  $("#p1,#p2,#p4,#p5,#p6").css("border", "none");
+  $("#p1,#p2,#p4,#p5,#p6").css("opacity", "1");
+
+  $("#bestsel_3").css("display", "block");
+  $("#bestsel_2, #bestsel_1, #bestsel_4, #bestsel_5, #bestsel_6").css("display", "none");
+});
+$("#p4").click(function (event) {
+  $("#p4").css("border-right", "1px solid #d8dce0");
+  $("#p4").css("border-bottom", "4px solid #81bd66");
+  $("#p4").css("opacity", "0.8");
+  $("#p1,#p3,#p2,#p5,#p6").css("border", "none");
+  $("#p1,#p3,#p2,#p5,#p6").css("opacity", "1");
+
+  $("#bestsel_4").css("display", "block");
+  $("#bestsel_2, #bestsel_3, #bestsel_1, #bestsel_5, #bestsel_6").css("display", "none");
+});
+$("#p5").click(function (event) {
+  $("#p5").css("border-right", "1px solid #d8dce0");
+  $("#p5").css("border-bottom", "4px solid #81bd66");
+  $("#p5").css("opacity", "0.8");
+  $("#p1,#p3,#p4,#p2,#p6").css("border", "none");
+  $("#p1,#p3,#p4,#p2,#p6").css("opacity", "1");
+
+  $("#bestsel_5").css("display", "block");
+  $("#bestsel_2, #bestsel_3, #bestsel_4, #bestsel_1, #bestsel_6").css("display", "none");
+});
+$("#p6").click(function (event) {
+  $("#p6").css("border-right", "1px solid #d8dce0");
+  $("#p6").css("border-bottom", "4px solid #81bd66");
+  $("#p6").css("opacity", "0.8");
+  $("#p1,#p3,#p4,#p5,#p2").css("border", "none");
+  $("#p1,#p3,#p4,#p5,#p2").css("opacity", "1");
+
+  $("#bestsel_6").css("display", "block");
+  $("#bestsel_2, #bestsel_3, #bestsel_4, #bestsel_5, #bestsel_1").css("display", "none");
+});
+
 
 //mainslider
 $(document).ready(function () {
